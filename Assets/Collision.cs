8c98c3515d729fda631fb58ea906d2738a2fd18d
Seyeon Lee <seyeon040768@gmail.com>
2023-04-24 01:06:45 +0900

    add collision system

---
+++ Assets/Collision.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Hit!");
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Triggered!");
+    }
+}
